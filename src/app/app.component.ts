25a2966a96ea3da694730a47d38898b047297509
ST300814 <juanjo_300814@outlook.com>
2023-08-11 11:54:43 -0600

    Crud de ANgular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
 
+import  { Employee } from './models/employee';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ejercicios-front';
+
+  employeeArray: Employee[] =[
+    {id:1, name: "Eliot", country:"USA"},
+    {id:2, name: "Eithan", country:"Canada"},
+    {id:3, name: "Ely", country:"Mexico"}
+
+  ];
+
+  selectedEmployee: Employee = new Employee ();
+
+  openForEdit(employee: Employee){
+    this.selectedEmployee = employee;
+  }
+
+  addOrEdit(){
+
+    if(this.selectedEmployee.id == 0){
+      this.selectedEmployee.id = this.employeeArray.length + 1;
+      this.employeeArray.push(this.selectedEmployee);
+    }
+
+      this.selectedEmployee = new Employee();
+  }
+
+  delete(){
+    if(confirm('Are you sure you want to delete ir?')){
+    this.employeeArray = this.employeeArray.filter(x => x!= this.selectedEmployee);
+    this.selectedEmployee = new Employee ();
+    }
+  }
+
 }
